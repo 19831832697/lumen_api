8bce132657a0ecbf58f8fbdc92903468564ffdad
fengdan <2128516529@qq.com>
2019-05-18 16:16:31 +0800

    支付

--- app/Http/Controllers/alipay/AlipayController.php
+++ app/Http/Controllers/alipay/AlipayController.php
@@ -11,6 +11,7 @@ class AlipayController extends Controller
 {
     public function zPay(Request $request){
         $order_no=$request->input('orderno');
+//        echo $order_no;die;
         $dataInfo=DB::table('shop_order')->where('order_no',$order_no)->first();
         $order_id=$dataInfo->order_id;
 
@@ -19,12 +20,9 @@ class AlipayController extends Controller
         $ch=curl_init($url);
         curl_setopt($ch,CURLOPT_URL,$url);
         curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
-//        curl_setopt($ch,CURLOPT_POST,1);
-//        curl_setopt($ch,CURLOPT_POSTFIELDS,$order_id);
-//        curl_setopt($ch,CURLOPT_HTTPHEADER,['Content-Type:text/plain']);
         $res=curl_exec($ch);
 //        $errno=curl_errno($ch);
 //        echo $errno;
-        echo $res;
+        var_dump($res);
     }
 }
