0971346b87b22ae003194951d3ca26d0b13801ee
fengdan <2128516529@qq.com>
2019-05-18 16:30:05 +0800

    支付

--- app/Http/Controllers/alipay/AlipayController.php
+++ app/Http/Controllers/alipay/AlipayController.php
@@ -11,19 +11,23 @@ class AlipayController extends Controller
 {
     public function zPay(Request $request){
         $order_no=$request->input('orderno');
-//        echo $order_no;die;
+        echo $order_no;die;
         $dataInfo=DB::table('shop_order')->where('order_no',$order_no)->first();
-        var_dump($dataInfo);die;
-        $order_id=$dataInfo->order_id;
-
-
-        $url="http://passport.ffddd.top/zPay?order_id=$order_id";
-        $ch=curl_init($url);
-        curl_setopt($ch,CURLOPT_URL,$url);
-        curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
-        $res=curl_exec($ch);
+        if($dataInfo){
+            $order_id=$dataInfo->order_id;
+            $url="http://passport.ffddd.top/zPay?order_id=$order_id";
+            $ch=curl_init($url);
+            curl_setopt($ch,CURLOPT_URL,$url);
+            curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
+            $res=curl_exec($ch);
 //        $errno=curl_errno($ch);
 //        echo $errno;
-        var_dump($res);
+            var_dump($res);
+        }
+        var_dump($dataInfo);die;
+
+
+
+
     }
 }
